c1196471f3379cb768a63d365e18d93e76076274
TranVanTienDat <ckim32993@gmail.com>
2023-11-11 16:49:00 +0700

    d

--- Back-end/middlewares/upload.middleware.js
+++ Back-end/middlewares/upload.middleware.js
@@ -1,9 +1,26 @@
-// middleware.js
+// // middleware.js
+// const multer = require("multer");
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// module.exports = upload;
+
 const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    const uploadPath = path.join(__dirname, "uploads/");
+    cb(null, uploadPath);
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
