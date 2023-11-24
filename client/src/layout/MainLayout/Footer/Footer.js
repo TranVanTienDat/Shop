8ec932e094fdfd97a8116a85142dd3955b9d1eaa
TranVanTienDat <ckim32993@gmail.com>
2023-11-24 20:48:49 +0700

    Add

--- client/src/layout/MainLayout/Footer/Footer.js
+++ client/src/layout/MainLayout/Footer/Footer.js
@@ -1,52 +1,66 @@
 import classNames from 'classnames/bind';
 import images from '~/assets/images';
 import styles from './Footer.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function Footer() {
   return (
     <div className={cx('footer')}>
-      <div className={cx('view')}>
-        <div className={cx('customer')}>
-          <img
-            className={cx('img')}
-            src="https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lfalvvg3pjdj5e"
-            alt=""
-          />
-          <div className={cx('element')}>
-            <div className={cx('colum')}>
-              <img className={cx('element__img')} src={images.logo} alt="" />
-
-              <h4 className={cx('text')}>Privacy Policy</h4>
-              <h4 className={cx('text')}>Terms and Condition</h4>
+      <div className={cx('inner')}>
+        <div className={cx('brand')}>
+          <img className={cx('logo')} src={images.logoWhite} alt="" />
+          <p className={cx('text')}>
+            Funding freemium long tail hypotheses first mover advantage assets
+            ownership
+          </p>
+          <div className={cx('contact')}>
+            <div className={cx('box')}>
+              <FontAwesomeIcon icon={faEnvelope} className={cx('icon')} />
+              <span className={cx('title')}>kimca@mail.com</span>
             </div>
-            <h4 className={cx('text-end')}>
-              @2020 TanahAir Studio. All rights reserved.
-            </h4>
+
+            <div className={cx('box')}>
+              <FontAwesomeIcon icon={faPhone} className={cx('icon')} />
+              <span className={cx('title')}>+089 736 655</span>
+            </div>
           </div>
         </div>
-        <div className={cx('Social-Network')}>
-          <img
-            className={cx('icon')}
-            src="https://cdn-icons-png.flaticon.com/128/3677/3677211.png"
-            alt=""
-          />
-          <img
-            className={cx('icon')}
-            src="https://cdn-icons-png.flaticon.com/128/408/408707.png"
-            alt=""
-          />
-          <img
-            className={cx('icon')}
-            src="https://cdn-icons-png.flaticon.com/128/3256/3256013.png"
-            alt=""
-          />
-          <img
-            className={cx('icon')}
-            src="https://cdn-icons-png.flaticon.com/128/2504/2504965.png"
-            alt=""
-          />
+
+        <div className={cx('info')}>
+          <div className={cx('column')}>
+            <span className={cx('title')}>Services</span>
+            <ul>
+              <li className={cx('text')}>Web Hosting</li>
+              <li className={cx('text')}>Domains</li>
+              <li className={cx('text')}>Premium Hosting</li>
+              <li className={cx('text')}>Private Server</li>
+              <li className={cx('text')}>E-mail Hosting</li>
+            </ul>
+          </div>
+
+          <div className={cx('column')}>
+            <span className={cx('title')}>Services</span>
+            <ul>
+              <li className={cx('text')}>Web Hosting</li>
+              <li className={cx('text')}>Domains</li>
+              <li className={cx('text')}>Premium Hosting</li>
+              <li className={cx('text')}>Private Server</li>
+              <li className={cx('text')}>E-mail Hosting</li>
+            </ul>
+          </div>
+          <div className={cx('column')}>
+            <span className={cx('title')}>Services</span>
+            <ul>
+              <li className={cx('text')}>Web Hosting</li>
+              <li className={cx('text')}>Domains</li>
+              <li className={cx('text')}>Premium Hosting</li>
+              <li className={cx('text')}>Private Server</li>
+              <li className={cx('text')}>E-mail Hosting</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
